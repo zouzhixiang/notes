e6c22fe8dfbce54475a022c3c82ab84a91ee0655
zouzhixiang <zouzhixiang@supcon.com>
2021-12-05 17:10:31 +0800

    notes init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: '笔记',
+    base: '/notes/',
+    themeConfig: {
+        nav: [
+            { 'text': '前端', link: '/fe/' },
+            { 'text': '后端', link: '/fb/' },
+            { 'text': '工具', link: '/tools/' }
+        ]
+    }
+}
