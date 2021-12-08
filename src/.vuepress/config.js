ae12f18789a85592d1eecfbc0c4647f312018731
zouzhixiang <zouzhixiang@supcon.com>
2021-12-08 22:13:58 +0800

    patch

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,12 +1,27 @@
 module.exports = {
-    title: '笔记',
-    base: '/notes/',
-    dest: './docs',
-    themeConfig: {
-        nav: [
-            { 'text': '前端', link: '/fe/' },
-            { 'text': '后端', link: '/fb/' },
-            { 'text': '工具', link: '/tools/' }
-        ]
+  title: '笔记',
+  base: '/notes/',
+  dest: './docs',
+  displayAllHeaders: true,
+  themeConfig: {
+    nav: [
+      { text: '前端', link: '/fe/' },
+      { text: '后端', link: '/fb/' },
+      { text: '工具', link: '/tools/' }
+    ],
+    siderbar: {
+      '/tools/': [
+        {
+          title: 'vscode',
+          sidebarDepth: 0,
+          path: '/tools/vscode'
+        },
+        {
+          title: 'git',
+          sidebarDepth: 0,
+          path: '/tools/git'
+        }
+      ]
     }
+  }
 }
---
+++ src/tools/vscode.md
@@ -0,0 +1,9 @@
+# vscode 无法控制台无法执行命令
+
+<https://www.cnblogs.com/kugar/p/12630178.html>
+
+```powershell
+# 管理员身份运行
+get-ExecutionPolicy
+set-ExecutionPolicy RemoteSigned
+```
